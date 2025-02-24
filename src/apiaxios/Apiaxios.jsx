0398fe615cb178f304acac7a8e69b088dd10f688
Eman Gaber <emangaber413@gmail.com>
2025-02-24 20:56:00 +0200

    Resolve1

--- src/apiaxios/Apiaxios.jsx
+++ src/apiaxios/Apiaxios.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const axiosinstance = axios.create({
     "baseURL":"https://newsapi.org/v2/",
-    "method":"GET",
+    
     "params": {
         "apiKey": "9431564532014a098939f9e0305a4984"  
     }
